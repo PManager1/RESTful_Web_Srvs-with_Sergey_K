684c5c2401c075b8895233f5472047e601d8c380
Jay <jpca999@gmail.com>
2023-05-04 22:13:21 -0400

    users controller workign as per sergey

--- app-mobile-dev/src/main/java/appmobiledev/ui/controller/HelloController.java
+++ app-mobile-dev/src/main/java/appmobiledev/ui/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping( "/hello" )
     public String echo() {
-        return "Hello World! - from Hello contro";
+        return "Hello World! - from Hello controller";
     }
 
 }
--- app-mobile-dev/src/main/java/appmobiledev/ui/controller/UserController.java
+++ app-mobile-dev/src/main/java/appmobiledev/ui/controller/UserController.java
@@ -3,13 +3,26 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+//@RequestMapping("users")
 public class UserController {
 
-    @RequestMapping( "/user" )
-    public String echo() {
-        return "Hello World! usr cont";
+    @GetMapping("/users")
+    public String getUser(){
+        return "hello Get user method was called";
+    }
+
+    @PostMapping
+    public String createUser(){
+        return "create user  called";
+    }
+    @PutMapping
+    public String updateUser (){
+        return "update user was called ";
+    }
+    @DeleteMapping
+    public String deleteUser(){
+        return "delete user was called";
     }
 
 }
-
 
